0a8bd2bf063875d5c1d0c9b557587b53eceb3ee6
kpzaolod6000 <43651853+kpzaolod6000@users.noreply.github.com>
2022-06-26 13:25:58 -0500

    sky not moving

--- Assets/Scripts/SkiboxMove.cs
+++ Assets/Scripts/SkiboxMove.cs
@@ -6,10 +6,23 @@ public class SkiboxMove : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] public float skyboxSpeed = 1f;
+    [SerializeField] public float timeMultiplier = 1;
+    [SerializeField] public GameObject sun;
+    [HideInInspector] public Light sunLight;
 
+    void Start()
+    {
+        //sun = gameObject;
+        //sunLight = gameObject.GetComponent<Light>();
+    }
     // Update is called once per frame
     void Update()
     {
-        RenderSettings.skybox.SetFloat("_Rotation", Time.time * skyboxSpeed);
+        //RenderSettings.skybox.SetFloat("_Rotation", Time.time * skyboxSpeed);
+        //SunUpdate();
+    }
+    public void SunUpdate()
+    {
+        sun.transform.localEulerAngles = new Vector3(Time.time * timeMultiplier, -30, 0);
     }
 }
